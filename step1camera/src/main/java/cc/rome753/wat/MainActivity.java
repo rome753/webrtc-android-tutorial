599bf3091b25301dc1fe78eaad56a997848d4e0b
chao <chenrongchao@lbesec.com>
2019-01-28 15:39:09 +0800

    Init project

---
+++ step1camera/src/main/java/cc/rome753/wat/MainActivity.java
@@ -0,0 +1,13 @@
+package cc.rome753.wat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
