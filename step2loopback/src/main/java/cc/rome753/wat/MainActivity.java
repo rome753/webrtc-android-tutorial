1be23a3885b95ab4f139105d1c0c6d375328af25
chao <rome753@163.com>
2020-01-20 17:31:39 +0800

    Use AndroidX.

--- step2loopback/src/main/java/cc/rome753/wat/MainActivity.java
+++ step2loopback/src/main/java/cc/rome753/wat/MainActivity.java
@@ -1,7 +1,8 @@
 package cc.rome753.wat;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.webrtc.Camera1Enumerator;
 import org.webrtc.DefaultVideoDecoderFactory;
--- step4multipeers/src/main/java/cc/rome753/wat/MainActivity.java
+++ step4multipeers/src/main/java/cc/rome753/wat/MainActivity.java
@@ -1,7 +1,7 @@
 package cc.rome753.wat;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
 import org.webrtc.Camera1Enumerator;
